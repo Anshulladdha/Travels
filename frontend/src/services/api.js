45328b2958c01dd2f29ac38593503b8652a7a537
Anshul Laddha <anshulladdha2001@gmail.com>
2025-05-13 14:52:09 +0530

    updated api

---
+++ frontend/src/services/api.js
@@ -0,0 +1,12 @@
+const BASE_URL = process.env.REACT_APP_BASE_URL;
+
+export const registerUser = async (userData) => {
+  const response = await fetch(`${BASE_URL}/register`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(userData),
+  });
+  return response.json();
+};
